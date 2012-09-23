4556c0e25993c3c2eea618b87d5126a0ecb4cf5b
Franz <Franz@localhost>
2012-09-23 11:19:20 +0200

    added method

--- CellMapper/src/de/locked/cellmapper/model/Utilities.java
+++ CellMapper/src/de/locked/cellmapper/model/Utilities.java
@@ -1,7 +1,9 @@
 package de.locked.cellmapper.model;
 
+import android.app.Activity;
 import android.content.Context;
 import android.provider.Settings;
+import android.view.WindowManager;
 
 public class Utilities {
     /**
@@ -14,13 +16,19 @@ public class Utilities {
         return Settings.System.getInt(context.getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) != 0;
 
     }
-    
-    public static  String rpad(String s, int l, String str) {
+
+    public static String rpad(String s, int l, String str) {
         while (s.length() < l) {
             s += str;
         }
         return s;
     }
-    
 
+    public static void setKeepScreenOn(Activity activity, boolean keepScreenOn) {
+        if (keepScreenOn) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
 }
